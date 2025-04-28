aa2087396cf2073ed2f93c0e7574ad1a7bfe562d
acuraels <daniil.rib@mail.ru>
2025-04-28 13:34:05 +0500

    gh-pages-sipport-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Game } from './components/Game/Game.jsx'
 
 
 export default function App() {
-  return <Game />
+  return (
+    <BrowserRouter basename='/tic-tac-toe/'>
+      <Routes>
+        <Route path="/" element={<Navigate to="/game" />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </BrowserRouter>
+  );
+
 }
